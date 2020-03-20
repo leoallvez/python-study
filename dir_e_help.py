bc82c127f07b77e43d1135ea0f81a51d6da33627
Leonardo Alves <leonardo.alves@clarifysistemas.com.br>
2020-03-20 00:44:04 -0300

    dir e help

---
+++ dir_e_help.py
@@ -0,0 +1,12 @@
+"""
+    Utilitários Python para auxliar na programação;
+
+    dir  -> Apresenta todos os atributos/propriedades e funções/método disponiveis
+    para um determinado tipo de dados ou variável.
+    exemplo: dir(tipo de dado / variavel)
+
+    help -> Apresenta a documentação/ como utilizar os atributos/propriedades e funções disponiveis
+    para determinado tipo de dado ou variavel.
+    exemplo: help(tipo de dado/variavel.propriedade)
+
+"""
